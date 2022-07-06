5fd788123840dc2dab198003e7a4351013ef1f66
John Bridges <jbridges373@gmail.com>
2022-07-06 11:17:34 +0100

    heading animation added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,21 @@ function App() {
 
   return (
     <div className="App">
+    
+      <motion.h1      
+      animate={{
+            scale: [0, 1, 1, 2, 1],
+            rotate: [0, 0, 2, -5, 5, -2, 0, 360, -360, 0],
+            borderRadius: ["20%", "20%", "50%", "50%", "20%"]
+          }}
+          transition={{
+            duration: 2,
+            ease: "easeInOut",
+            times: [0, 0.2, 0.5, 0.8, 1],
+            repeat: 2,
+            repeatDelay: 1
+          }}
+      className="heading">IMAGE CAROUSEL</motion.h1>
       <motion.div ref={carousel} className="carousel" whileTap={{cursor: "grabbing"}}>
         <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} 
         className="inner-carousel">
